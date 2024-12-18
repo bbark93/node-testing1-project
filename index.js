292d88f05af0da87ef159c3bbea55f2496b140ff
Brandon Barksdale <bbark93@gmail.com>
2024-12-17 19:52:46 -0500

    wip

--- index.js
+++ index.js
@@ -188,8 +188,9 @@ class Car {
  *    // result is false
  * })
  */
-function isEvenNumberAsync(number) {
+async function isEvenNumberAsync(number) {
   // ✨ implement
+  return number % 2 === 0 || false
 }
 
 module.exports = {
