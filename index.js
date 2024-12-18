86289a4251a32dc080af90aee9968ca751ebbedf
Brandon Barksdale <bbark93@gmail.com>
2024-12-17 19:37:51 -0500

    wip

--- index.js
+++ index.js
@@ -123,6 +123,8 @@ class Car {
     this.odometer = 0 // car initilizes with zero miles
     this.tank = tankSize // car initiazes full of gas
     // ✨ initialize whatever other properties are needed
+    this.tankSize = tankSize
+    this.mpg = mpg
   }
 
   /**
@@ -140,6 +142,15 @@ class Car {
    */
   drive(distance) {
     // ✨ implement
+    const range = this.tank * this.mpg
+    if (distance <= range) {
+      this.odometer = this.odometer + distance
+      this.tank = this.tank - (distance / this.mpg)
+    } else {
+      this.odometer = this.odometer + range
+      this.tank = 0
+    }
+    return this.odometer
   }
 
   /**
@@ -155,6 +166,12 @@ class Car {
    */
   refuel(gallons) {
     // ✨ implement
+    if (gallons <= this.tankSize -this.tank) {
+      this.tank = this.tank + gallons
+    } else {
+      this.tank = this.tankSize
+    }
+    return this.tank * this.mpg
   }
 }
 
